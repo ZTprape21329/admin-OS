0aac493f0786a7b3bdde7c0a5279baebdc7aa325
曾韬 <your email>
2020-01-02 18:25:32 +0800

    修改查询语句

--- server/router/list.js
+++ server/router/list.js
@@ -3,7 +3,12 @@ const pool = require('../pool.js');
 let router = express.Router();
 router.get('/userList',(req, res) =>{
     let obj = req.query;
-    let sql = 'SELECT uname, name, sex, time, email, address FROM dbuser.userlist';
+    let sql = 'SELECT id, uname, name, sex, time, email, address FROM dbuser.userlist';
+    // let sqlList = {
+    //     select: `SELECT uname, name, sex, time, email, address FROM dbuser.userlist`,
+    //     insert: `INSERT INTO dbuser.userlist(NULL,uname, name, sex, time, email, address) VALUES (${uname}, ${name}, ${sex}, ${time}, ${email}, ${address})`,
+    //     update: `UPDATE dbuser.userlist SET uname = '${uname}', name = '${name}', sex = '${sex}', time = '${time}', address = '${address}' WHERE id = ${id}` 
+    // };
     pool.query(sql,[],(err, result) => {
         if(err) throw err;
         res.send(result)
